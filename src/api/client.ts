f4182901420b6b9b2397dced4b56c435b14e999b
ned-razzz <smg9972@gmail.com>
2025-10-07 12:44:24 +0900

    fix: environment variable setting

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-// 백엔드 서버의 주소를 설정합니다.
-// 로컬 개발 환경에서는 package.json에 "proxy": "http://localhost:8080"를 추가하는 방법도 유용합니다.
+// 백엔드 서버의 주소
+const API_URL = import.meta.env.VITE_BACKEND_API_URL;
+
 const apiClient = axios.create({
-    baseURL: "/api", // 프록시 설정을 사용하거나 실제 백엔드 주소를 입력
+    baseURL: API_URL,
     headers: {
         "Content-Type": "application/json",
     },
--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,4 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss(), tanstackRouter()],
-    server: {
-        proxy: {
-            // '/api'로 시작하는 모든 요청을 백엔드 서버로 전달하라는 의미
-            "/api": {
-                // 실제 백엔드 서버의 주소
-                target: "http://localhost:8080",
-
-                // cross-origin 요청을 위해 필수로 추가
-                changeOrigin: true,
-            },
-        },
-    },
 });
